1b609e85f8ff0ad308911a68d99fcda476680ed8
meet <shiraz19@meet.mit.edu>
2018-08-07 11:10:33 -0400

    lol

--- examples/example4.py
+++ examples/example4.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# Flask app with template and variables
+
 @app.route('/')
 def hello_world(year='y2'):
     return render_template("hello_vars.html",
--- exercises/app.py
+++ exercises/app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template("index.html")
+	player = ["Aya aka greatest sport player", "Your mom", "Eilon", "Shiraz"]
+	likes_same_sport=True
+	return render_template("index.html",
+	player=player,
+	likes_same_sport=likes_same_sport
+	
+	)
 
 if __name__ == '__main__':
 	app.run(debug = True)
-home_page()
